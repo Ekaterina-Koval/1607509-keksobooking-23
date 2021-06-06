ced3de744bad70167c0f7b211e084d7ac5374b2c
Ekaterina <m.ek@mail.ru>
2021-06-07 02:43:55 +0300

    Генерация временных данных (без учета уникальности)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
+// Утилитарные функции
 const getInteger = (min, max) => {
-  //если переданы отрицательные значения
-  if (min < 0 || max < 0) {
+  if (min < 0) {
     return 'Ошибка ввода данных: минимальное/максимальное значения ожидаются положительные числа (включая ноль)';
   }
-
-  //если передать значение «до» меньшее, чем значение «от», или равное ему
   if (min >= max) {
     return 'Ошибка ввода данных: минимальное значение ожидается меньше максимального';
   }
-
-  //если передан диапазон, в котором отсутствует целое положительное число
   if (Math.floor(min) === Math.floor(max)) {
     return 'Ошибка ввода данных: в переданном дапазоне отсутствует целое число';
   }
@@ -18,40 +13,108 @@ const getInteger = (min, max) => {
   return Math.ceil(Math.random() * (max - min) + min);
 };
 
-getInteger();
-
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 const getFloat = (min, max, fixed) => {
-  if (min < 0 || max < 0) {
+  if (min < 0) {
     return 'Ошибка ввода данных: минимальное/максимальное значения ожидаются положительные числа (включая ноль)';
   }
-
-  //если «количество знаков после запятой» передано не целое число
   if (fixed < 0 || !Number.isInteger(fixed)) {
     return 'Ошибка ввода данных: количество знаков после запятой ожидается целое положительное число (включая ноль)';
   }
-
-  if (min < 0 || max < 0) {
-    return 'Ошибка ввода данных: минимальное/максимальное значения ожидаются положительные числа (включая ноль)';
-  }
-
   if (min >= max) {
     return 'Ошибка ввода данных: минимальное значение ожидается меньше максимального';
   }
-
-  //если после округления сгенерированного числа, оно перестаёт попадать в диапазон
   if (min.toFixed(fixed) === max.toFixed(fixed)) {
     return 'Ошибка ввода данных: необходимо увеличить количество знаков после запятой или изменить диапазон значений';
   }
-
-  return Math.abs((Math.random() * (max - min) + min).toFixed(fixed));
+  return (Math.random() * (max - min) + min).toFixed(fixed);
 };
 
-getFloat();
+// Генерация временных данных
+const TITLE = [
+  'Лучшее предложение! Спешите забронировать до конца недели.',
+  'Комфорт и уют наше кредо. Почувствуйте себя как дома',
+  'Остановившись у нас, вы не забудете Токио никогда!',
+  'Предложение для самых требовательных',
+  'Cashback 10% и экскурсия на выбор в подарок',
+  'Бронируйте сейчас - платите потом.',
+  'Незабываемый отдых в лучшем районе столицы Японии',
+  'Великолепный вид из окна, удобное расположение',
+  'Отличная цена! Вам понравится наше предложение!',
+  'Не возможно удержаться от того, чтобы не вернуться ещё раз',
+];
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTION = [
+  'Хорошее место для доступа в любую точку Токио, но это очень тихий, чистый и безопасный жилой район.',
+  'Очень красивая территория с детскими и спортивными площадками.',
+  'Тихий, безопасный и красивый жилой район, недалеко от центра города на поезде с удобным транспортным сообщением.',
+  'У нас есть дополнительный велосипед для гостей, чтобы они могли кататься по окрестностям или на вокзал.',
+  'Комната реконструирована в прошлом году, компромисс между японским и западным стилями, на стене висит картина.',
+  'Здесь так красиво в сезон полного цветения сакуры.',
+  'Дорога к станции, по обе стороны реки обсажена вишневыми деревьями.',
+  'Полностью отремонтированное, меблированное высококачественное жильё в центре Токио. Полностью оборудованная кухня и ванные комнаты.',
+  'Тихое и спокойное место ночью, но активная и привлекательная зона днем.',
+  'Многочисленные магазины, различные рестораны, и супермаркет всего в 1 минуте, это идеальное место, чтобы пережить опыт жителя Токио, наслаждаясь доступом к близлежащим туристическим достопримечательностям.',
+];
 
-/*
-Используемые источники:
-  https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  https://learn.javascript.ru/number
-  https://habr.com/ru/post/312880/
-*/
+const getRandomArrayElement = (elements) => elements[getInteger(0, elements.length - 1)];
+const createRandomArray = (array) => new Array(getInteger(0, array.length - 1)).fill(null).map(() => array[getInteger(0, array.length - 1)]);
+
+const createAdvertisement = () => {
+  const userAvatar = `img/avatars/user0${getInteger(0, 10)}.png`;
+  const locationX = getFloat(35.65000, 35.70000, 5);
+  const locationY = getFloat(139.7, 139.8, 5);
+  return {
+    author: userAvatar,
+    offer: {
+      title: getRandomArrayElement(TITLE),
+      address: `${locationX}, ${locationY}`,
+      price: getInteger(0, 1000000),
+      type: getRandomArrayElement(TYPE),
+      rooms: getInteger(1, 3),
+      guests: getInteger(1, 6),
+      checkin: getRandomArrayElement(CHECKIN),
+      checkout: getRandomArrayElement(CHECKOUT),
+      features: createRandomArray(FEATURES),
+      description: getRandomArrayElement(DESCRIPTION),
+      photos: createRandomArray(PHOTOS),
+    },
+    location: {
+      lat: locationX,
+      lng: locationY,
+    },
+  };
+};
+
+const advertisementListLength = 10;
+const advertisementList = new Array(advertisementListLength).fill(null).map(() => createAdvertisement());
+
+advertisementList;
